3e8f298d2a01180593b4fa415fc6f57fad648ef3
ewgenius <ewgeniux@gmail.com>
2015-01-20 22:05:56 +0300

    upd

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,11 @@
-(function (document) {
+(function(document) {
   'use strict';
 
-  document.addEventListener('polymer-ready', function () {
+  document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     //console.log('Polymer is ready to rock!');
   });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+  // wrap document so it plays nice with other libraries
+  // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
